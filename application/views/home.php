b06314301af37439d9667c4151846f2f1b863a96
manavgoyal53 <39936373+manavgoyal53@users.noreply.github.com>
2019-02-17 17:09:39 +0530

    Changes

--- application/views/home.php
+++ application/views/home.php
@@ -11,8 +11,8 @@
     <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
         <a href="<?php echo  base_url(); ?>site/hod.html">
             <div class="info-box blue-bg">
-                <div class="count">Salary</div>
-                <div class="title">Manage salary</div>
+                <div class="count">Head of Department</div>
+                <div class="title">Manage HOD</div>
             </div><!--/.info-box-->
         </a>
     </div><!--/.col-->
