3b6e296d4639b5f66cdfa96abccac9e732721c1f
Sahil Kumar <sahil.ms@outlook.com>
2019-02-17 15:58:47 +0530

    Update welcome_message.php

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -18,7 +18,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
   <div class="collapse navbar-collapse" id="navbarColor01">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="#">Home<span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="#">Features</a>
@@ -34,7 +34,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
   <h1 class="display-3">Hello,</h1>
   <p class="lead">Our approach is to create a portal which will comprise of all details of MSME, their departments and their respective employees. It will contain the details of the payments made to each department and also the salary payments made by each department to the employees. Employees will have their unique ID at the MSME level. Hence, even after shifting to another department, the employee data will be accessible from the company’s database as it will be department independent. Each department will also have their own database which will contain necessary information about the employee, like their PF and Pension accounts, their salaries and any other relevant data which the department requires. Employees will be able to view their past salaries, make rebate/allowance claim. All Employee fund accounts are associated with the Employee ID, and this information is available to their respective department. The salary is given to the employees after deduction, and the deduction data is stored in Department Database and synced with MSME main database.</p>
   <hr class="my-4">
-  <p>To login, click on the buttons below.</p>
+  <p>To login, click on the buttons below. Happy Coding</p>
   <p class="lead">
   <button type="button" class="btn btn-primary">Admin</button>
   <button type="button" class="btn btn-primary">HOD</button>
