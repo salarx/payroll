d14982586b65b5f9a7d43eb4581632e305fc50e3
manavgoyal53 <39936373+manavgoyal53@users.noreply.github.com>
2019-02-17 19:13:30 +0530

    sjcgg

--- application/controllers/site.php
+++ application/controllers/site.php
@@ -22,8 +22,7 @@ class Site extends CI_Controller {
         $this->session->unset_userdata('username');
         $data['message'] = 'You are successfully logged out';
         $this->session->set_userdata($data);
-
-        $this->load->view('login');
+        redirect('admin/login',"refresh");
     }
 
     public function index(){
