cac33a3a971de39de002b6a2e0a7075f67128587
manavgoyal53 <39936373+manavgoyal53@users.noreply.github.com>
2019-02-19 03:03:55 +0530

    Database changed

--- payroll.sql
+++ payroll.sql
@@ -2,10 +2,10 @@
 -- version 4.8.4
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Generation Time: Feb 18, 2019 at 07:37 PM
--- Server version: 5.7.24
--- PHP Version: 7.2.14
+-- Host: 127.0.0.1
+-- Generation Time: Feb 18, 2019 at 10:32 PM
+-- Server version: 10.1.37-MariaDB
+-- PHP Version: 7.3.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -28,14 +28,12 @@ SET time_zone = "+00:00";
 -- Table structure for table `admin`
 --
 
-DROP TABLE IF EXISTS `admin`;
-CREATE TABLE IF NOT EXISTS `admin` (
-  `admin_id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `admin` (
+  `admin_id` int(11) NOT NULL,
   `admin_username` varchar(30) NOT NULL,
   `admin_password` varchar(128) NOT NULL,
-  `admin_salt` varchar(60) NOT NULL,
-  PRIMARY KEY (`admin_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+  `admin_salt` varchar(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `admin`
@@ -51,22 +49,19 @@ INSERT INTO `admin` (`admin_id`, `admin_username`, `admin_password`, `admin_salt
 -- Table structure for table `departments`
 --
 
-DROP TABLE IF EXISTS `departments`;
-CREATE TABLE IF NOT EXISTS `departments` (
+CREATE TABLE `departments` (
   `dep_id` int(11) NOT NULL,
   `dep_name` text NOT NULL,
   `password` varchar(128) DEFAULT NULL,
-  `employee_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`dep_id`),
-  KEY `employee_id` (`employee_id`)
+  `emp_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `departments`
 --
 
-INSERT INTO `departments` (`dep_id`, `dep_name`, `password`, `employee_id`) VALUES
-(1, 'Production', NULL, NULL),
+INSERT INTO `departments` (`dep_id`, `dep_name`, `password`, `emp_id`) VALUES
+(1, 'Production', 'manav', 102),
 (2, 'Research and Development', NULL, NULL),
 (3, 'Marketing', NULL, NULL),
 (4, 'Purchasing', NULL, NULL),
@@ -78,11 +73,9 @@ INSERT INTO `departments` (`dep_id`, `dep_name`, `password`, `employee_id`) VALU
 -- Table structure for table `designation`
 --
 
-DROP TABLE IF EXISTS `designation`;
-CREATE TABLE IF NOT EXISTS `designation` (
+CREATE TABLE `designation` (
   `id` int(11) NOT NULL,
-  `designation` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`)
+  `designation` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -107,9 +100,8 @@ INSERT INTO `designation` (`id`, `designation`) VALUES
 -- Table structure for table `employee`
 --
 
-DROP TABLE IF EXISTS `employee`;
-CREATE TABLE IF NOT EXISTS `employee` (
-  `employee_id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `employee` (
+  `employee_id` int(11) NOT NULL,
   `payment_id` int(11) NOT NULL,
   `status_id` int(11) NOT NULL,
   `employee_name` varchar(30) NOT NULL,
@@ -119,19 +111,16 @@ CREATE TABLE IF NOT EXISTS `employee` (
   `employee_address` text NOT NULL,
   `employee_salary` tinyint(1) NOT NULL DEFAULT '0',
   `emp_password` varchar(128) DEFAULT NULL,
-  PRIMARY KEY (`employee_id`),
-  KEY `status_id` (`status_id`),
-  KEY `payment_id` (`payment_id`),
-  KEY `employee_department` (`employee_department`),
-  KEY `employee_designation` (`employee_designation`)
-) ENGINE=InnoDB AUTO_INCREMENT=103 DEFAULT CHARSET=utf8;
+  `employee_email` varchar(128) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `employee`
 --
 
-INSERT INTO `employee` (`employee_id`, `payment_id`, `status_id`, `employee_name`, `employee_department`, `employee_designation`, `employee_phone`, `employee_address`, `employee_salary`, `emp_password`) VALUES
-(102, 1, 1, 'Manav Goyal', 1, 1, '+91-84493924', 'sfb', 1, 'asd123');
+INSERT INTO `employee` (`employee_id`, `payment_id`, `status_id`, `employee_name`, `employee_department`, `employee_designation`, `employee_phone`, `employee_address`, `employee_salary`, `emp_password`, `employee_email`) VALUES
+(102, 1, 1, 'Manav Goyal', 1, 1, '+91-84493924', 'sfb', 1, 'asd123', NULL),
+(103, 1, 1, 'Sahil Kumar', 1, 1, '+91-94670173', 'PDPM IIITDMJ', 0, 'sahil', 'salarx.gm@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -139,12 +128,10 @@ INSERT INTO `employee` (`employee_id`, `payment_id`, `status_id`, `employee_name
 -- Table structure for table `payment`
 --
 
-DROP TABLE IF EXISTS `payment`;
-CREATE TABLE IF NOT EXISTS `payment` (
-  `payment_id` int(11) NOT NULL AUTO_INCREMENT,
-  `payment_type` varchar(12) NOT NULL,
-  PRIMARY KEY (`payment_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+CREATE TABLE `payment` (
+  `payment_id` int(11) NOT NULL,
+  `payment_type` varchar(12) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `payment`
@@ -161,16 +148,13 @@ INSERT INTO `payment` (`payment_id`, `payment_type`) VALUES
 -- Table structure for table `salary`
 --
 
-DROP TABLE IF EXISTS `salary`;
-CREATE TABLE IF NOT EXISTS `salary` (
-  `salary_id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `salary` (
+  `salary_id` int(11) NOT NULL,
   `employee_id` int(11) NOT NULL,
   `salary_basic` varchar(10) NOT NULL,
   `salary_overtime` varchar(10) NOT NULL,
-  `salary_other` varchar(10) NOT NULL,
-  PRIMARY KEY (`salary_id`),
-  KEY `employee_id` (`employee_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+  `salary_other` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `salary`
@@ -185,12 +169,10 @@ INSERT INTO `salary` (`salary_id`, `employee_id`, `salary_basic`, `salary_overti
 -- Table structure for table `status`
 --
 
-DROP TABLE IF EXISTS `status`;
-CREATE TABLE IF NOT EXISTS `status` (
-  `status_id` int(11) NOT NULL AUTO_INCREMENT,
-  `status_name` varchar(10) NOT NULL,
-  PRIMARY KEY (`status_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+CREATE TABLE `status` (
+  `status_id` int(11) NOT NULL,
+  `status_name` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `status`
@@ -202,6 +184,92 @@ INSERT INTO `status` (`status_id`, `status_name`) VALUES
 (3, 'Probation');
 
 --
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`admin_id`);
+
+--
+-- Indexes for table `departments`
+--
+ALTER TABLE `departments`
+  ADD PRIMARY KEY (`dep_id`),
+  ADD KEY `employee_id` (`emp_id`);
+
+--
+-- Indexes for table `designation`
+--
+ALTER TABLE `designation`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `employee`
+--
+ALTER TABLE `employee`
+  ADD PRIMARY KEY (`employee_id`),
+  ADD KEY `status_id` (`status_id`),
+  ADD KEY `payment_id` (`payment_id`),
+  ADD KEY `employee_department` (`employee_department`),
+  ADD KEY `employee_designation` (`employee_designation`);
+
+--
+-- Indexes for table `payment`
+--
+ALTER TABLE `payment`
+  ADD PRIMARY KEY (`payment_id`);
+
+--
+-- Indexes for table `salary`
+--
+ALTER TABLE `salary`
+  ADD PRIMARY KEY (`salary_id`),
+  ADD KEY `employee_id` (`employee_id`);
+
+--
+-- Indexes for table `status`
+--
+ALTER TABLE `status`
+  ADD PRIMARY KEY (`status_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `employee`
+--
+ALTER TABLE `employee`
+  MODIFY `employee_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=104;
+
+--
+-- AUTO_INCREMENT for table `payment`
+--
+ALTER TABLE `payment`
+  MODIFY `payment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `salary`
+--
+ALTER TABLE `salary`
+  MODIFY `salary_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `status`
+--
+ALTER TABLE `status`
+  MODIFY `status_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- Constraints for dumped tables
 --
 
@@ -209,7 +277,7 @@ INSERT INTO `status` (`status_id`, `status_name`) VALUES
 -- Constraints for table `departments`
 --
 ALTER TABLE `departments`
-  ADD CONSTRAINT `departments_ibfk_1` FOREIGN KEY (`employee_id`) REFERENCES `employee` (`employee_id`);
+  ADD CONSTRAINT `departments_ibfk_1` FOREIGN KEY (`emp_id`) REFERENCES `employee` (`employee_id`);
 
 --
 -- Constraints for table `employee`
