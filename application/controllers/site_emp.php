ea15aa0cadb79e3ed2daa7720e35eab61ef2af7f
Sahil Kumar <sahil.ms@outlook.com>
2019-02-19 02:55:23 +0530

    Update site_emp.php

--- application/controllers/site_emp.php
+++ application/controllers/site_emp.php
@@ -34,48 +34,6 @@ class Site_emp extends CI_Controller {
         $this->load->view('master',$data);
     }
 
-    public function employee(){
-
-        $data = array();
-        $data["title"] = "Employee";
-        $data["heading"] = "Employee Details";
-        $data["base_url"] = base_url() . "site_emp/employee";
-        $data["total_rows"] = $this->employee_model->record_count();
-        $data["per_page"] = 20;
-        $data["uri_segment"] = 3;
-
-        $this->pagination->initialize($data);
-        $page = ($this->uri->segment(3)) ? $this->uri->segment(3) : 0;
-
-        $data["results"] = $this->employee_model->fetch_employee($data["per_page"], $page);
-
-        $data["links"] = $this->pagination->create_links();
-
-        $data["content"] = $this->load->view('employee',$data,true);
-        $this->load->view('master',$data);
-    }
-
-    public function hod(){
-
-        $data = array();
-        $data["title"] = "Department";
-        $data["heading"] = "Department Head Details";
-        $data["base_url"] = base_url() . "site_emp/hod";
-        $data["total_rows"] = $this->hod_model->record_count();
-        $data["per_page"] = 20;
-        $data["uri_segment"] = 3;
-
-        $this->pagination->initialize($data);
-        $page = ($this->uri->segment(3)) ? $this->uri->segment(3) : 0;
-
-        $data["results"] = $this->hod_model->fetch_hod($data["per_page"], $page);
-
-        $data["links"] = $this->pagination->create_links();
-
-        $data["content"] = $this->load->view('hod',$data,true);
-        $this->load->view('master',$data);
-    }
-
     public function salary(){
 
         $data = array();
