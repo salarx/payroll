3b1064f74db131ec4919a560a6e38f9d534a57cd
manavgoyal53 <39936373+manavgoyal53@users.noreply.github.com>
2019-03-02 11:32:14 +0530

    Added slip view for employee.

---
+++ application/views/slip_emp.php
@@ -0,0 +1,24 @@
+<div class="row">
+    <div class="col-lg-6">
+        <section class="panel">
+            <div class="panel-heading text-center">Payment Slip</div>
+            <div class="panel-body">
+                <table class="table">
+                    <tr>
+                        <td>Transaction ID: #<?php echo $result->transaction_id; ?></td>
+                    </tr>
+                    <tr>
+                        <td>Employee Name: <?php echo $result->dep_name; ?></td>
+                    </tr>
+                    <tr>
+                        <td>Account Type: <?php echo $result->type_name; ?></td>
+                    </tr>
+                    <tr>
+                        <td>Amount: <?php echo $result->amount; ?></td>
+                    </tr>
+                </table>
+            </div>
+            <div class="panel-footer"></div>
+        </section>
+    </div>
+</div>
