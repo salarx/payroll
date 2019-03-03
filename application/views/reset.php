57dd62e9ab578c9506819196ab52871eea531c50
PalakMendiratta <46782458+PalakMendiratta@users.noreply.github.com>
2019-03-03 09:30:24 +0530

    reset

--- application/views/reset.php
+++ application/views/reset.php
@@ -1,17 +1,19 @@
-<!-- Main content -->
+<br><br><br>
 <?php $category=$this->session->userdata('category'); ?>
 <section class="content">
+  <div class="container jumbotron">
     <div class="box box-default">
         <div class="box-header with-border">
         <?php if($category==1) { ?>
-            <h3 class="box-title">Admin Profile</h3>
+            <h3 class="box-title"><center><b>Admin Profile</b></center></h3>
             <?php } ?>
             <?php if($category==2) { ?>
-            <h3 class="box-title">MSME Profile</h3>
+            <h3 class="box-title"><center><b>MSME Profile</b></center></h3>
             <?php } ?>
             <?php if($category==3) { ?>
-            <h3 class="box-title">Employee Profile</h3>
+            <h3 class="box-title"><center><b>Employee Profile</b></center></h3>
             <?php } ?>
+            <br><br>
         </div>
         <div class="box-body">
             <div class="col-xs-5">
@@ -21,18 +23,19 @@
                 <form action="<?php echo base_url(); ?>site_msme/reset_password.html" method="post"><?php } ?>
                 <?php if($category==3) { ?>
                 <form action="<?php echo base_url(); ?>site_emp/reset_password.html" method="post"><?php } ?>
-                    <div class="form-group has-feedback">
-                        <label for="Password">Password</label>
-                        <input type="password" name="password" class="form-control" placeholder="Password" required/>
+                    <div class="form-group has-feedback input-group">
+                        <label class="col-lg-2" for="Password"><center><b>Password</b></center></label>
+                        <input type="password" name="password" class="form-control col-lg-6" placeholder="Password" required/>
                     </div>
-                    <div class="row">
+
                         <div class="col-xs-12">
-                            <button type="submit" class="btn btn-primary pull-right">Reset</button>
-                        </div><!-- /.col -->
+                          <label class="col-lg-7"></label>
+                            <button type="submit" class="btn btn-primary col-lg-1">Reset</button>
+                        <!-- /.col -->
                     </div>
                 </form>
             </div>
         </div><!-- /.box-body -->
-        <div class="box-footer"></div>
+        <div class="box-footer"></div></div>
     </div><!-- /.box -->
 </section><!-- /.content -->
