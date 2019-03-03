93899ac4ce009777c03083699256bd7d47fa3d12
PalakMendiratta <46782458+PalakMendiratta@users.noreply.github.com>
2019-03-03 12:29:31 +0530

    gbd

--- application/views/home_admin.php
+++ application/views/home_admin.php
@@ -22,31 +22,16 @@
          <a href="<?php echo  base_url(); ?>site/msme.html">
            <div class="card card-body" style="max-width: 20rem; background: #080076; color: #ffffff">
              <i class="icon_datareport"></i>
-          <center>   <h4 class="card-title">HOD</h4>
-             <p class="card-text">Manage HOD</p></center><!--/.info-box-->
+          <center>   <h4 class="card-title">MSME</h4>
+             <p class="card-text">Manage MSME</p></center><!--/.info-box-->
        </div><!--/.info-box-->
         </a>
       </div>
     <!--/.col-->
 </div>
 
-<br><br><br>
-    <div class="row">
-      <div class="col-lg-4 col-md-4"></div>
-    <!--/.col-->&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp
-    <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-            <a href="<?php echo  base_url(); ?>site/settings.html">
-                <div class="card card-body" style="max-width: 20rem; background: #080076; color: #ffffff">
-            <i class="icon_cog"></i>
-            <center><h4 class="card-title">SETTINGS</h4>
-            <p class="card-text">Manage Settings</p></center><!--/.info-box-->
-      <!--/.info-box-->
-      </div>
-        </a>
+<br><br><br><br><br>
 
-    </div><!--/.col-->
-
-   </div>
 <?php } ?>
 <br> <br> <br> <br> <br> <br>
 <hr class="my-4">
