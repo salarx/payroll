84171654646324c075193e95a9b3f8554d04921f
PalakMendiratta <46782458+PalakMendiratta@users.noreply.github.com>
2019-03-03 12:25:00 +0530

    xfvdf

--- application/views/add_employee.php
+++ application/views/add_employee.php
@@ -1,43 +1,50 @@
+<br><br>
 <?php $category=$this->session->userdata('category'); ?>
+<div class="container jumbotron">
+
 <div class="row">
     <div class="col-lg-12">
         <section class="panel">
             <div class="panel-body">
+              <h3><center><b><u> ADD EMPLOYEE</u></b></center></h3><br><br>
                 <div class="form">
                     <form class="form-validate form-horizontal" id="feedback_form" method="post" action="<?php echo base_url(); ?>employee/add_employee_commit.html">
-                        <div class="form-group">
-                            <label for="name" class="control-label col-lg-2">Full Name *</label>
+                        <div class="form-group input-group">
+                            <label for="name" class="control-label col-lg-2"><b>Full Name *</b></label>
                             <div class="col-lg-10">
                                 <input class="form-control" name="name" type="text" required/>
                             </div>
                         </div>
-                        <div class="form-group">
-                            <label for="phone" class="control-label col-lg-2">Phone *</label>
+                        <div class="form-group input-group">
+                            <label for="phone" class="control-label col-lg-2"><b>Phone *</b></label>
                             <div class="col-lg-10">
                                 <input class="form-control" name="phone" maxlength="10" type="text" required/>
                             </div>
                         </div>
-                        <div class="form-group">
-                            <label for="email" class="control-label col-lg-2">E-mail</label>
+                        <div class="form-group input-group">
+                            <label for="email" class="control-label col-lg-2"><b>E-mail</b></label>
                             <div class="col-lg-10">
                                 <input class="form-control" name="email" type="email"></input>
                             </div>
                         </div>
-                        <div class="form-group">
-                            <label for="salary" class="control-label col-lg-2">Salary</label>
+                        <div class="form-group input-group">
+                            <label for="salary" class="control-label col-lg-2"><b>Salary</b></label>
                             <div class="col-lg-10">
                                 <input class="form-control" name="salary" type="number"></input>
                             </div>
                         </div>
-                        <div class="form-group">
-                            <label for="password" class="control-label col-lg-5"><b>Password (A lowercase letter a capital (uppercase) letter and a number. Minimum 8 characters)</b></label>
+                        <div class="form-group input-group">
+                            <label for="password" class="control-label col-lg-2"><b>Password </b></label>
                             <div class="col-lg-10">
                                 <input class="form-control" type="password" name="password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" required></input>
+                                <label style="color:red">(Password must be 8 to 12 character long.It must contain at least one lowercase letter, a capital (uppercase) letter and a number.)</label>
                             </div>
                         </div>
 
                         <div class="form-group">
-                            <div class="col-lg-offset-2 col-lg-10">
+                            <div class="col-lg-offset-2 ">
+                              &nbsp &nbsp &nbsp &nbsp 
+                              <label class="col-lg-11"></label>
                                 <button class="btn btn-primary" type="submit">Add</button>
                             </div>
                         </div>
@@ -46,4 +53,5 @@
             </div>
         </section>
     </div>
+</div>
 </div>
