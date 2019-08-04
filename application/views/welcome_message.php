7f2820f7edc88ad0311ab16bdf4184e7101e2b2a
Manav <manavgoyal53@gmail.com>
2019-08-05 00:22:06 +0530

    lol

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -80,6 +80,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 </div>
 </body>
 
+
 <!-- jQuery library -->
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 
